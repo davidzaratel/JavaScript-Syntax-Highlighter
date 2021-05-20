8af5bb9facd8d4f76de4019e1cb24b36fb91160b
David Zarate Lopez <A01329785@itesm.mx>
2021-05-20 00:22:04 -0500

    Adding index.html

--- lib/integradora.ex
+++ lib/integradora.ex
@@ -5,21 +5,31 @@ defmodule Integradora do
   end
 
   def compare(:num,value), do: "<span class=numint>" <> (value |> to_string) <> "</span>"
+  def compare(:float,value), do: "<span class=numint>" <> (value |> to_string) <> "</span>"
   def compare(:identifier,value), do: "<span class=identifier>" <> (value |> to_string) <> "</span>"
   def compare(:reserved,value), do: "<span class=reserved>" <> (value |> to_string) <> "</span>"
   def compare(:enter,_), do: "<br>"
   def compare(:commentline,value), do: "<span class=commentline>" <> (value |> to_string) <> "</span><br>"
   def compare(:signs,value), do: "<span class=signs>" <> (value |> to_string) <> "</span>"
   def compare(:space,value), do: "<span class=space>" <> (value |> to_string) <> "</span>"
+  def compare(:string,value), do: "<span class=string>" <> (value |> to_string) <> "</span>"
 
 
   #hacer una funcion a llamar dentro del enum.map, la cual compare cual palabra es
   def convertir do
-    :lexer.string(leerJs())
+    {:ok, file} = File.open("index.html",[:write])
+    IO.write(file," <link rel='stylesheet' href='styles.css'>")
+    IO.write(file,"<pre>")
+    IO.write(file,"<code>")
+    html = :lexer.string(leerJs())
     |> elem(1)
     |> Enum.map(fn {token,_,value} ->
      compare(token,value)
     end)
     |>Enum.join()
+    IO.write(file,html)
+    IO.write(file,"</code>")
+    IO.write(file,"</pre>")
   end
+
 end
